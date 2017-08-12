55f439fa232bf7fb8cb1bcaa3ac11ce2937f5e8d
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-08-12 12:43:53 -0600

    Added tests surround promise fulfillment and rejection for xml() and xmlmin().

--- test/xml.test.js
+++ test/xml.test.js
@@ -3,7 +3,27 @@ let should = require('should');
 
 describe('xml', () => {
 
-    it('should exist', () => {
-        should.exist(SweetData.xml);
-    });
+  it('should exist', () => {
+    should.exist(SweetData.xml);
+  });
+
+  it('should return a promise', () => {
+    const xml = '<?xml version="1.0" encoding="UTF-8" ?>      <!DOCTYPE foo SYSTEM "Foo.dtd"><a>          <b>bbb</b>   <!-- comment --><c/><d><soapenv:Envelope xmlns:soapenv="http://xxx" xmlns:xsd="http://yyy" xmlns:xsi="http://zzz"></soapenv>       </d><e>        <![CDATA[ <z></z> ]]></e><f><g></g></f></a>';
+
+    should(SweetData.xml(xml)).be.a.Promise();
+  });
+
+  it('should fulfill the promise on valid xml input', () => {
+    const xml = '<?xml version="1.0" encoding="UTF-8" ?>      <!DOCTYPE foo SYSTEM "Foo.dtd"><a>          <b>bbb</b>   <!-- comment --><c/><d><soapenv:Envelope xmlns:soapenv="http://xxx" xmlns:xsd="http://yyy" xmlns:xsi="http://zzz"></soapenv>       </d><e>        <![CDATA[ <z></z> ]]></e><f><g></g></f></a>';
+
+    should(SweetData.xml(xml)).be.fulfilled();
+  });
+
+  it('should reject the promise on invalid xml input with TypeError', () => {
+    const invalidxml = 123;
+
+    const typeError = new TypeError('Input must of type "object" or "string"');
+
+    should(SweetData.xml(invalidxml)).be.rejectedWith(typeError);
+  });
 });
