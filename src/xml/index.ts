29550fa25d018572a0c86ba95fc46a210a4c21c3
Alex Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2017-12-12 15:32:22 -0700

    Transformed xml and xmlmin to TypeScript.

--- src/xml/index.ts
+++ src/xml/index.ts
@@ -1,71 +1,77 @@
-var Promise = require('promise-polyfill');
-var setAsap = require('setasap');
-Promise._immediateFn = setAsap;
-
-function xml(text) {
-  var that = this;
-
-  return new Promise(function(resolve, reject) {
+export const xml = (text: string) => {
+  return new Promise(function (resolve, reject) {
     try {
-      var ar = text.replace(/>\s{0,}</g,"><")
-      .replace(/</g,"~::~<")
-      .replace(/xmlns\:/g,"~::~xmlns:")
-      .replace(/xmlns\=/g,"~::~xmlns=")
-      .split('~::~'),
-      len = ar.length,
-      inComment = false,
-      deep = 0,
-      str = '',
-      ix = 0;
+      let ar = text.replace(/>\s{0,}</g, "><")
+        .replace(/</g, "~::~<")
+        .replace(/xmlns\:/g, "~::~xmlns:")
+        .replace(/xmlns\=/g, "~::~xmlns=")
+        .split('~::~'),
+        len = ar.length,
+        inComment = false,
+        deep = 0,
+        str = '',
+        ix = 0;
 
-      for(ix=0;ix<len;ix++) {
+      for (ix = 0; ix < len; ix++) {
         // start comment or <![CDATA[...]]> or <!DOCTYPE //
-        if(ar[ix].search(/<!/) > -1) {
-          str += that.shift[deep]+ar[ix];
+        if (ar[ix].search(/<!/) > -1) {
+          str += this.shift[deep] + ar[ix];
           inComment = true;
           // end comment  or <![CDATA[...]]> //
-          if(ar[ix].search(/-->/) > -1 || ar[ix].search(/\]>/) > -1 || ar[ix].search(/!DOCTYPE/) > -1 ) {
+          if (ar[ix].search(/-->/) > -1 || ar[ix].search(/\]>/) > -1 || ar[ix].search(/!DOCTYPE/) > -1) {
             inComment = false;
           }
-        } else
-        // end comment  or <![CDATA[...]]> //
-        if(ar[ix].search(/-->/) > -1 || ar[ix].search(/\]>/) > -1) {
-          str += ar[ix];
-          inComment = false;
-        } else
-        // <elm></elm> //
-        if( /^<\w/.exec(ar[ix-1]) && /^<\/\w/.exec(ar[ix]) &&
-          /^<[\w:\-\.\,]+/.exec(ar[ix-1]) == /^<\/[\w:\-\.\,]+/.exec(ar[ix])[0].replace('/','')) {
-          str += ar[ix];
-          if(!inComment) deep--;
-        } else
-          // <elm> //
-        if(ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) == -1 && ar[ix].search(/\/>/) == -1 ) {
-          str = !inComment ? str += that.shift[deep++]+ar[ix] : str += ar[ix];
-        } else
-          // <elm>...</elm> //
-        if(ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) > -1) {
-          str = !inComment ? str += that.shift[deep]+ar[ix] : str += ar[ix];
-        } else
-        // </elm> //
-        if(ar[ix].search(/<\//) > -1) {
-          str = !inComment ? str += that.shift[--deep]+ar[ix] : str += ar[ix];
-        } else
-        // <elm/> //
-        if(ar[ix].search(/\/>/) > -1 ) {
-          str = !inComment ? str += that.shift[deep]+ar[ix] : str += ar[ix];
-        } else
-        // <? xml ... ?> //
-        if(ar[ix].search(/<\?/) > -1) {
-          str += that.shift[deep]+ar[ix];
-        } else
-        // xmlns //
-        if( ar[ix].search(/xmlns\:/) > -1  || ar[ix].search(/xmlns\=/) > -1) {
-          str += that.shift[deep]+ar[ix];
-        }
+        } else {
+          // end comment  or <![CDATA[...]]> //
+          if (ar[ix].search(/-->/) > -1 || ar[ix].search(/\]>/) > -1) {
+            str += ar[ix];
+            inComment = false;
+          } else {
+            const a = /^<\w/.exec(ar[ix - 1]) && /^<\/\w/.exec(ar[ix]);
+            const b = /^<[\w:\-\.\,]+/.exec(ar[ix - 1]) as any;
+            const c = /^<\/[\w:\-\.\,]+/.exec(ar[ix])[0].replace('/', '');
+            // <elm></elm> //
+            if (a && (b == c)) {
+              str += ar[ix];
+              if (!inComment) {
+                deep--;
+              }
+            } else {
+              // <elm> //
+              if (ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) === -1 && ar[ix].search(/\/>/) === -1) {
+                str = !inComment ? str += this.shift[deep++] + ar[ix] : str += ar[ix];
+              } else {
+                // <elm>...</elm> //
+                if (ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) > -1) {
+                  str = !inComment ? str += this.shift[deep] + ar[ix] : str += ar[ix];
+                } else {
+                  // </elm> //
+                  if (ar[ix].search(/<\//) > -1) {
+                    str = !inComment ? str += this.shift[--deep] + ar[ix] : str += ar[ix];
+                  } else {
+                    // <elm/> //
+                    if (ar[ix].search(/\/>/) > -1) {
+                      str = !inComment ? str += this.shift[deep] + ar[ix] : str += ar[ix];
+                    } else {
+                      // <? xml ... ?> //
+                      if (ar[ix].search(/<\?/) > -1) {
+                        str += this.shift[deep] + ar[ix];
+                      } else {
+                        // xmlns //
+                        if (ar[ix].search(/xmlns\:/) > -1 || ar[ix].search(/xmlns\=/) > -1) {
+                          str += this.shift[deep] + ar[ix];
+                        }
 
-        else {
-          str += ar[ix];
+                        else {
+                          str += ar[ix];
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
       }
 
@@ -74,21 +80,18 @@ function xml(text) {
       reject(e);
     }
   });
-}
+};
 
-function xmlmin(text, preserveComments) {
-
-  return new Promise(function(resolve, reject) {
+export const xmlmin = (text: string, preserveComments: boolean) => {
+  return new Promise(function (resolve, reject) {
     try {
-      var str = preserveComments ? text
-            : text.replace(/\<![ \r\n\t]*(--([^\-]|[\r\n]|-[^\-])*--[ \r\n\t]*)\>/g,"");
+      const str = preserveComments ? text : text.replace(/\<![ \r\n\t]*(--([^\-]|[\r\n]|-[^\-])*--[ \r\n\t]*)\>/g, "");
 
-      resolve(str.replace(/>\s{0,}</g,"><"));
+      resolve(str.replace(/>\s{0,}</g, "><"));
     } catch (e) {
       reject(e);
     }
   });
-}
+};
 
 exports.xml = xml;
-exports.xmlmin = xmlmin;
