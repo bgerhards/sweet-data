d15de47d1ad1bad7775e56c13adcddfea9a8be7b
Alex Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2017-12-12 14:33:44 -0700

    Transformed CSS functions to TypeScript.

--- src/css/index.ts
+++ src/css/index.ts
@@ -1,47 +1,43 @@
-function css(text) {
+export const css = (text: string) => {
+  let ar = text.replace(/\s{1,}/g, ' ')
+    .replace(/\{/g, "{~::~")
+    .replace(/\}/g, "~::~}~::~")
+    .replace(/\;/g, ";~::~")
+    .replace(/\/\*/g, "~::~/*")
+    .replace(/\*\//g, "*/~::~")
+    .replace(/~::~\s{0,}~::~/g, "~::~")
+    .split('~::~'),
+    len = ar.length,
+    deep = 0,
+    str = '',
+    ix = 0;
 
-    var ar = text.replace(/\s{1,}/g,' ')
-          .replace(/\{/g,"{~::~")
-          .replace(/\}/g,"~::~}~::~")
-          .replace(/\;/g,";~::~")
-          .replace(/\/\*/g,"~::~/*")
-          .replace(/\*\//g,"*/~::~")
-          .replace(/~::~\s{0,}~::~/g,"~::~")
-          .split('~::~'),
-      len = ar.length,
-      deep = 0,
-      str = '',
-      ix = 0;
+  for (ix = 0; ix < len; ix++) {
 
-      for(ix=0;ix<len;ix++) {
-
-        if( /\{/.exec(ar[ix]))  {
-          str += this.shift[deep++]+ar[ix];
-        } else
-        if( /\}/.exec(ar[ix]))  {
-          str += this.shift[--deep]+ar[ix];
-        } else
-        if( /\*\\/.exec(ar[ix]))  {
-          str += this.shift[deep]+ar[ix];
+    if (/\{/.exec(ar[ix])) {
+      str += this.shift[deep++] + ar[ix];
+    } else
+      if (/\}/.exec(ar[ix])) {
+        str += this.shift[--deep] + ar[ix];
+      } else
+        if (/\*\\/.exec(ar[ix])) {
+          str += this.shift[deep] + ar[ix];
         }
         else {
-          str += this.shift[deep]+ar[ix];
+          str += this.shift[deep] + ar[ix];
         }
-      }
-      return str.replace(/^\n{1,}/,'');
   }
 
-  function cssmin(text, preserveComments) {
+  return str.replace(/^\n{1,}/, '');
+};
 
-    var str = preserveComments ? text
-             : text.replace(/\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+\//g,"") ;
-    return str.replace(/\s{1,}/g,' ')
-          .replace(/\{\s{1,}/g,"{")
-          .replace(/\}\s{1,}/g,"}")
-          .replace(/\;\s{1,}/g,";")
-          .replace(/\/\*\s{1,}/g,"/*")
-          .replace(/\*\/\s{1,}/g,"*/");
-  }
-
-  exports.css = css;
-  exports.cssmin = cssmin;
+export const cssmin = (text: string, preserveComments: boolean) => {
+  const str = preserveComments ? text
+    : text.replace(/\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+\//g, "");
+  return str.replace(/\s{1,}/g, ' ')
+    .replace(/\{\s{1,}/g, "{")
+    .replace(/\}\s{1,}/g, "}")
+    .replace(/\;\s{1,}/g, ";")
+    .replace(/\/\*\s{1,}/g, "/*")
+    .replace(/\*\/\s{1,}/g, "*/");
+};
